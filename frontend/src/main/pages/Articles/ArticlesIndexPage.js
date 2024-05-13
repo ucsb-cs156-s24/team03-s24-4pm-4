1a7c8b50c3b05eabd728947134ad3f499f6065c3
tihdiR <46095500+tihdiR@users.noreply.github.com>
2024-05-13 14:15:53 -0700

    Update ArticlesIndexPage.js

--- frontend/src/main/pages/Articles/ArticlesIndexPage.js
+++ frontend/src/main/pages/Articles/ArticlesIndexPage.js
@@ -44,18 +44,3 @@ export default function ArticlesIndexPage() {
     );
 }
 
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-
-export default function ArticlesIndexPage() {
-
-  // Stryker disable all : placeholder for future implementation
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p><a href="/placeholder/create">Create</a></p>
-        <p><a href="/placeholder/edit/1">Edit</a></p>
-      </div>
-    </BasicLayout>
-  )
-}
