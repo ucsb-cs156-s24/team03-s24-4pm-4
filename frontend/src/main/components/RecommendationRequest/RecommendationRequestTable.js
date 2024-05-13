e3590439ab764937b267b805151d1f973e329d72
Jaren Lowe <jaren@ucsb.edu>
2024-05-13 02:36:52 -0700

    jl - Added RecRequest index page implementation and changed util file to work properly

--- frontend/src/main/components/RecommendationRequest/RecommendationRequestTable.js
+++ frontend/src/main/components/RecommendationRequest/RecommendationRequestTable.js
@@ -19,7 +19,7 @@ export default function RecommendationRequestTable({ recommendationrequests, cur
     const deleteMutation = useBackendMutation(
         cellToAxiosParamsDelete,
         { onSuccess: onDeleteSuccess },
-        ["/api/recommendationrequest/all"]
+        ["/api/recommendationrequests/all"]
     );
     // Stryker restore all 
 
