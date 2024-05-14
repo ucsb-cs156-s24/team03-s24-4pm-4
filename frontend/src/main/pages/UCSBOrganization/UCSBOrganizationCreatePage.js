9a1c56768997a54b6ebde69b354e4ac810db4932
emrecikisir <emrecikisir@ucsb.edu>
2024-05-13 17:45:48 -0700

    Copied and Tested Organization Placeholders

---
+++ frontend/src/main/pages/UCSBOrganization/UCSBOrganizationCreatePage.js
@@ -0,0 +1,13 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function UCSBOrganizationCreatePage() {
+
+  // Stryker disable all : placeholder for future implementation
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Nothing Here Yet to Create</h1>
+      </div>
+    </BasicLayout>
+  )
+}
---
+++ frontend/src/main/pages/UCSBOrganization/UCSBOrganizationIndexPage.js
@@ -0,0 +1,15 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function UCSBOrganizationIndexPage() {
+
+  // Stryker disable all : placeholder for future implementation
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Nothing here yet for Index Page</h1>
+        <p><a href="/ucsborganization/create">Create Page</a></p> 
+        <p><a href="/ucsborganization/edit/1">Edit Page</a></p>
+      </div>
+    </BasicLayout>
+  )
+}
