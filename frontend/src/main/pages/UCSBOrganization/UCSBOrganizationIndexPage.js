cb37838b5184c2a5eae3d92226804df4012f191c
emrecikisir <emrecikisir@ucsb.edu>
2024-05-13 19:18:04 -0700

    Index Page implemented and fully tested

--- frontend/src/main/pages/UCSBOrganization/UCSBOrganizationIndexPage.js
+++ frontend/src/main/pages/UCSBOrganization/UCSBOrganizationIndexPage.js
@@ -1,15 +1,47 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import UCSBOrganizationTable from 'main/components/UCSBOrganization/UCSBOrganizationTable';
+import { useCurrentUser , hasRole} from 'main/utils/currentUser'
+import { Button } from 'react-bootstrap';
 
 export default function UCSBOrganizationIndexPage() {
 
-  // Stryker disable all : placeholder for future implementation
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Nothing here yet for Index Page</h1>
-        <p><a href="/ucsborganization/create">Create Page</a></p> 
-        <p><a href="/ucsborganization/edit/1">Edit Page</a></p>
-      </div>
-    </BasicLayout>
-  )
+    const currentUser = useCurrentUser();
+
+
+    const { data: UCSBOrganization, error: _error, status: _status } =
+        useBackend(
+            // Stryker disable next-line all : don't test internal caching of React Query
+            ["/api/UCSBOrganization/all"],
+            { method: "GET", url: "/api/UCSBOrganization/all" },
+            // Stryker disable next-line all : don't test default value of empty list
+            []
+        );
+
+
+    const createButton = () => {
+        if (hasRole(currentUser, "ROLE_ADMIN")) {
+            return (
+                <Button
+                    variant="primary"
+                    href="/UCSBOrganization/create"
+                    style={{ float: "right" }}>
+                    Create Your UCSB Organization
+                </Button>
+            )
+        } 
+    }
+
+
+    return (
+        <BasicLayout>
+            <div className="pt-2">
+                {createButton()}
+                <h1>UCSB Organization</h1>
+                <UCSBOrganizationTable organizations={UCSBOrganization} currentUser={currentUser} />
+            </div>
+        </BasicLayout>
+    );
 }
